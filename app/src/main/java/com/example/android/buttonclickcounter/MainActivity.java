a4a59fa4aeb715abc169836622105fb35c456f76
Trevor Brown <tab@servermeister.net>
2016-10-04 12:43:51 +1100

    Add Java code to count button clicks.

--- app/src/main/java/com/example/android/buttonclickcounter/MainActivity.java
+++ app/src/main/java/com/example/android/buttonclickcounter/MainActivity.java
@@ -1,13 +1,47 @@
 package com.example.android.buttonclickcounter;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.method.ScrollingMovementMethod;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText userInput;
+    private Button button;
+    private TextView textView;
+    private int numTimesClicked = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        userInput = (EditText) findViewById(R.id.editText);
+        button = (Button) findViewById(R.id.button);
+        textView = (TextView) findViewById(R.id.textView);
+        textView.setText("");
+        textView.setMovementMethod(new ScrollingMovementMethod());
+
+        View.OnClickListener ourOnClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                numTimesClicked++;
+                String result = "The button got tapped " + numTimesClicked + " time";
+
+                if (numTimesClicked != 1) {
+                    result += "s";
+                }
+
+                result += "\n";
+
+                textView.append(result);
+
+            }
+        };
+
+        button.setOnClickListener(ourOnClickListener);
     }
 }
